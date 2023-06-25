1e4fa6b1aa0bbc18d73b7b3e1050f839499dd859
Arseny <seniaslams@gmail.com>
2023-06-25 19:02:28 +0500

    Add data formatter

--- src/index.js
+++ src/index.js
@@ -1,40 +1,44 @@
-import fs from "node:fs"
 import AdmZip from "adm-zip"
 import iconv from "iconv-lite"
 import xml2js from "xml2js"
-import { pipeline } from "node:stream"
-import { promisify } from "node:util"
 import fetch from "node-fetch"
 
-const zipPath = "./zipAchive.zip"
-
-const downlodAndSave = async () => {
-  const streamPipeline = promisify(pipeline)
-
+const prepareBICDataForDBWhrite = async () => {
   const response = await fetch("http://www.cbr.ru/s/newbik")
-
-  if (!response.ok)
-    throw new Error(`unexpected response ${response.statusText}`)
-
-  return await streamPipeline(response.body, fs.createWriteStream(zipPath))
-}
-
-const extractAndRead = async () => {
-  const zip = new AdmZip(zipPath)
-  zip.extractAllTo("./")
-  const nameOfExtractedFile = zip.getEntries().reduce((acc, entry) => {
-    acc = entry.entryName
-    return acc
-  }, "")
-  const xmlData = fs.readFileSync(`./${nameOfExtractedFile}`)
-  const decodedData = iconv.decode(xmlData, "cp1251")
+  const arrayBuffer = await response.arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
+  const zip = new AdmZip(buffer)
+  const zipEntries = zip.getEntries()
+  let xmlData
+  for (const entry of zipEntries) {
+    xmlData = entry.getData().toString("binary")
+  }
+  const decodedXmlData = iconv
+    .decode(Buffer.from(xmlData, "binary"), "cp1251")
+    .toString()
   const parser = new xml2js.Parser({ explicitArray: false })
-  const parsedXml = await parser.parseStringPromise(decodedData)
-  console.log(parsedXml)
+  const parsedXml = await parser.parseStringPromise(decodedXmlData)
+
+  const bicDirectoryEntries = parsedXml.ED807.BICDirectoryEntry
+
+  const result = []
+
+  for (const entry of bicDirectoryEntries) {
+    const bic = entry["$"].BIC
+    const name = entry.ParticipantInfo["$"].NameP
+    const accounts = entry.Accounts
+
+    if (accounts && Array.isArray(accounts)) {
+      for (const account of accounts) {
+        const corrAccount = account["$"].Account
+        result.push({ bic, name, corrAccount })
+      }
+    }
+  }
+
+  console.log(JSON.stringify(result, "\t", 1))
+
+  return result
 }
 
-const getXmlData = () => {
-  downlodAndSave().then(() => extractAndRead())
-}
-
-getXmlData()
+prepareBICDataForDBWhrite()
